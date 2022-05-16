13cbee555cdb27ca161850834a1b9ee012b9bc3a
EvansMutwiri <mutwirievansm@gmail.com>
2022-05-16 12:45:57 +0300

    Using classes

--- src/Card.js
+++ src/Card.js
@@ -30,73 +30,73 @@ const Card = () => {
     <div className='card App-header'>
 
       <div className='flex justify-between space-x-6'>
-      <article className="overflow-hidden rounded-lg shadow-lg bg-white">
+        <article className="overflow-hidden rounded-lg shadow-lg bg-white">
 
-<a href="https://picsum.photos/600/400/?random=1">
-  <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random=1"></img>
-</a>
+          <a href="https://picsum.photos/600/400/?random=1">
+            <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random=1"></img>
+          </a>
 
-<header className="flex items-center justify-between leading-tight p-2 md:p-4">
-  <h1 className="text-lg">
-    <a className="no-underline hover:underline text-black" href="#">
-      Article Title: {card1.title}
-    </a>
-  </h1>
-  <p className="text-gray-800 text-sm">
-    {new Date().toDateString()}
-  </p>
-</header>
+          <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+            <h1 className="text-lg">
+              <a className="no-underline hover:underline text-black" href="#">
+                Article Title: {card1.title}
+              </a>
+            </h1>
+            <p className="text-gray-800 text-sm">
+              {new Date().toDateString()}
+            </p>
+          </header>
 
-<footer className="flex items-center justify-between leading-none p-2 md:p-4">
-  <a className="flex items-center no-underline hover:underline text-black" href="https://picsum.photos/32/32/?random">
-    <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random"></img>
-    <p className="ml-2 text-sm">
-      Author: {card1.author}
-    </p>
-  </a>
-  <button className="no-underline text-gray-700 hover:text-red-600" href="#">
-    <span className="hidden">Like</span>
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-      <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-    </svg>
-  </button>
-</footer>
+          <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+            <a className="flex items-center no-underline hover:underline text-black" href="https://picsum.photos/32/32/?random">
+              <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random"></img>
+              <p className="ml-2 text-sm">
+                Author: {card1.author}
+              </p>
+            </a>
+            <button className="no-underline text-gray-700 hover:text-red-600" href="#">
+              <span className="hidden">Like</span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+              </svg>
+            </button>
+          </footer>
 
-</article>
+        </article>
 
-<article className="overflow-hidden rounded-lg shadow-lg bg-white">
+        <article className="overflow-hidden rounded-lg shadow-lg bg-white">
 
-<a href="https://picsum.photos/600/400/?random">
-  <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random"></img>
-</a>
+          <a href="https://picsum.photos/600/400/?random">
+            <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random"></img>
+          </a>
 
-<header className="flex items-center justify-between leading-tight p-2 md:p-4">
-  <h1 className="text-lg">
-    <a className="no-underline hover:underline text-black" href="#">
-      Article Title: {card2.title}
-    </a>
-  </h1>
-  <p className="text-gray-800 text-sm">
-    {new Date().toDateString()}
-  </p>
-</header>
+          <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+            <h1 className="text-lg">
+              <a className="no-underline hover:underline text-black" href="#">
+                Article Title: {card2.title}
+              </a>
+            </h1>
+            <p className="text-gray-800 text-sm">
+              {new Date().toDateString()}
+            </p>
+          </header>
 
-<footer className="flex items-center justify-between leading-none p-2 md:p-4">
-  <a className="flex items-center no-underline hover:underline text-black" href="https://picsum.photos/32/32/?random=2">
-    <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random=2"></img>
-    <p className="ml-2 text-sm">
-      Author: {card2.author}
-    </p>
-  </a>
-  <button className="no-underline text-gray-700 hover:text-red-600">
-    <span className="hidden">Like</span>
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-      <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-    </svg>
-  </button>
-</footer>
+          <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+            <a className="flex items-center no-underline hover:underline text-black" href="https://picsum.photos/32/32/?random=2">
+              <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random=2"></img>
+              <p className="ml-2 text-sm">
+                Author: {card2.author}
+              </p>
+            </a>
+            <button className="no-underline text-gray-700 hover:text-red-600">
+              <span className="hidden">Like</span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+              </svg>
+            </button>
+          </footer>
 
-</article>
+        </article>
       </div>
     </div>
   )
